14e1d40ca68a7e9e94c4ae34ffbab8a145f1fd56
Ben Puckhaber <ben.puckhaber@gmail.com>
2022-01-09 15:38:00 -0600

    exercise/01.extra-1

--- src/index.exercise.js
+++ src/index.exercise.js
@@ -1,8 +1,20 @@
 import * as React from 'react'
 import ReactDOM from 'react-dom'
+import {Dialog} from '@reach/dialog'
 import {Logo} from './components/logo'
+import '@reach/dialog/styles.css'
+
+const MODAL_STATE = Object.freeze({
+  NONE: 'none',
+  LOGIN: 'login',
+  REGISTER: 'register',
+})
 
 function App() {
+  const [openModal, setOpenModal] = React.useState(MODAL_STATE.NONE)
+
+  const closeModal = () => setOpenModal(MODAL_STATE.NONE)
+
   return (
     <>
       <Logo width="80" height="80" />
@@ -10,7 +22,7 @@ function App() {
       <div>
         <button
           onClick={() => {
-            alert('Login Clicked')
+            setOpenModal(MODAL_STATE.LOGIN)
           }}
         >
           Login
@@ -19,12 +31,20 @@ function App() {
       <div>
         <button
           onClick={() => {
-            alert('Register Clicked')
+            setOpenModal(MODAL_STATE.REGISTER)
           }}
         >
           Register
         </button>
       </div>
+      <Dialog isOpen={openModal === MODAL_STATE.LOGIN}>
+        <button onClick={closeModal}>Close</button>
+        <h3>Login</h3>
+      </Dialog>
+      <Dialog isOpen={openModal === MODAL_STATE.REGISTER}>
+        <button onClick={closeModal}>Close</button>
+        <h3>Register</h3>
+      </Dialog>
     </>
   )
 }
