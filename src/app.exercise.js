190ad8a2afbd0c3ac72797520a4b4d60bbc3540a
Ben Puckhaber <ben.puckhaber@gmail.com>
2022-01-17 06:44:53 -0600

    exercise/04.extra-2

--- src/app.exercise.js
+++ src/app.exercise.js
@@ -6,6 +6,9 @@ import * as auth from 'auth-provider'
 import {AuthenticatedApp} from './authenticated-app'
 import {UnauthenticatedApp} from './unauthenticated-app'
 import {client} from './utils/api-client'
+import {useAsync} from './utils/hooks'
+import {FullPageSpinner} from './components/lib'
+import * as colors from './styles/colors'
 
 const getUser = async () => {
   const token = await auth.getToken()
@@ -18,21 +21,56 @@ const getUser = async () => {
 }
 
 function App() {
-  const [user, setUser] = React.useState(null)
+  const {
+    data: user,
+    setData,
+    error,
+    run,
+    isIdle,
+    isLoading,
+    isError,
+    isSuccess,
+  } = useAsync()
 
   React.useEffect(() => {
-    getUser().then(u => setUser(u))
-  }, [])
+    run(getUser())
+  }, [run])
 
-  const login = form => auth.login(form).then(u => setUser(u))
-  const register = form => auth.register(form).then(u => setUser(u))
-  const logout = form => auth.logout(form).then(() => setUser(null))
+  const login = form => auth.login(form).then(u => setData(u))
+  const register = form => auth.register(form).then(u => setData(u))
+  const logout = form => auth.logout(form).then(() => setData(null))
 
-  return user == null ? (
-    <UnauthenticatedApp login={login} register={register} />
-  ) : (
-    <AuthenticatedApp user={user} logout={logout} />
-  )
+  if (isIdle || isLoading) {
+    return <FullPageSpinner />
+  }
+
+  if (isError) {
+    return (
+      <div
+        css={{
+          color: colors.danger,
+          height: '100vh',
+          display: 'flex',
+          flexDirection: 'column',
+          justifyContent: 'center',
+          alignItems: 'center',
+        }}
+      >
+        <p>Uh oh... There's a problem. Try refreshing the app.</p>
+        <pre>{error.message}</pre>
+      </div>
+    )
+  }
+
+  if (isSuccess) {
+    if (user == null) {
+      return <UnauthenticatedApp login={login} register={register} />
+    } else {
+      return <AuthenticatedApp user={user} logout={logout} />
+    }
+  }
+
+  return null
 }
 
 export {App}
